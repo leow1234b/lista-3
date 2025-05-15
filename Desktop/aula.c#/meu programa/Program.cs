b9a88d2f53705ed0cff9e766f6cc31d041055e4c
leonardocadena <leonardo.cadenasantos@gmail.com>
2025-05-14 22:34:16 -0300

    aa

---
+++ Desktop/aula.c#/meu programa/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine()!;
+
+        Console.WriteLine("Digite sua idade:");
+        int idade = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Digite seu peso (em kg):");
+        double peso = double.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Digite sua altura (em metros):");
+        double altura = double.Parse(Console.ReadLine()!);
+
+        double imc = peso / (altura * altura);
+
+        Console.WriteLine("Olá, " + nome + "!");
+        Console.WriteLine("Seu IMC é: " + imc);
+    }
+}
